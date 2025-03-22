ffeadd1eccbfad34935f70cbcba83832e82aa407
Viktoriia Matiushko <vikamatuchko772@gmail.com>
2025-03-22 13:22:33 +0200

    pep8 changes

--- task1.py
+++ task1.py
@@ -3,11 +3,9 @@ from typing import Dict, Callable
 
 def caching_fibonacci() -> Callable[[int], int]:
   # Create dict for memoization(caching)
-
   cache: Dict[int, int] = {}
 
   # Calculate fibonacci number using recursion and memoization(caching)
-
   def fibonacci(n: int) -> int:
     if n <= 0:
       return 0
@@ -18,11 +16,11 @@ def caching_fibonacci() -> Callable[[int], int]:
 
     cache[n] = fibonacci(n-1) + fibonacci(n-2)
     return cache[n]
-  
   return fibonacci
 
-# Example of use
 
+# Example of use
 fib = caching_fibonacci()
 print(fib(10))  
 print(fib(15))  
+
--- task2.py
+++ task2.py
@@ -2,25 +2,24 @@ from typing import Callable, Generator
 import re
 
 
+# We can use typing Iterator[float] instead of Generator[] for same purposes
+# (we do not send or return any value)
 def generator_numbers(text: str) -> Generator[float, None, None]:
   # Find all numeric values in text using regex
-
   numbers = re.findall(r'\b\d+(?:\.\d+)?\b', text)
 
   # Transform numeric values into floats and yield them for further use
-
   for num in numbers:
     yield float(num)
 
 
-
 def sum_profit(text: str, func: Callable[[str], Generator[float, None, None]]) -> float:
   # Calculate sum of all all numeric values and return it
-
   return sum(func(text))
 
-# Example of use
 
+# Example of use
 text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
 total_income = sum_profit(text, generator_numbers)
 print(f"Загальний дохід: {total_income}")
+
